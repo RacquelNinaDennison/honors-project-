52375006eca4dab9a6ebb2abd0d0d9a9892aa8f7
RacquelNinaDennison <racquelnina@gmail.com>
2024-08-19 18:22:30 +0200

    Changed the bash

--- run-test.sh
+++ run-test.sh
@@ -12,18 +12,6 @@ mkdir -p "$output_dir_random"
 
 for i in $(seq 50 25 200); do
     for j in $(seq 10 5 70); do
-        output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear.txt"
-        clingo --outf=2 --quiet=1 "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
-        output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform.txt"
-        clingo --outf=2 --quiet=1  "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c uniform=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
-        output_file="${output_dir_random}/knowledge_base_${i}-${j}_random.txt"
-        clingo --outf=2 --quiet=1 "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c random=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
-    done
-done
-
-
-for i in $(seq 50 25 200); do
-    for j in $(seq 10 5 70); do
         output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear_tweety.txt"
         clingo --outf=2 --quiet=1 --configuration=tweety "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
         output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform_tweety.txt"
@@ -33,19 +21,16 @@ for i in $(seq 50 25 200); do
     done
 done
 
-
-
 for i in $(seq 50 25 200); do
     for j in $(seq 10 5 70); do
-        output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear.txt"
-        clingo --outf=2 --quiet=1  "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
-        output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform.txt"
-        clingo --outf=2 --quiet=1 "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c uniform=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
-        output_file="${output_dir_random}/knowledge_base_${i}-${j}_random.txt"
-        clingo --outf=2 --quiet=1  "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c random=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
+        output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear_jumpy.txt"
+        clingo --outf=2 --quiet=1 --configuration=jumpy "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
+        output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform_jumpy.txt"
+        clingo --outf=2 --quiet=1 --configuration=jumpy "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c uniform=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
+        output_file="${output_dir_random}/knowledge_base_${i}-${j}_random_jumpy.txt"
+        clingo --outf=2 --quiet=1 --configuration=jumpy "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c random=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
     done
 done
-
 
 
 for i in $(seq 50 25 200); do
@@ -71,5 +56,17 @@ for i in $(seq 50 25 200); do
     done
 done
 
+
+
+for i in $(seq 50 25 200); do
+    for j in $(seq 10 5 70); do
+        output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear.txt"
+        clingo --outf=2 --quiet=1 "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
+        output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform.txt"
+        clingo --outf=2 --quiet=1  "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c uniform=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
+        output_file="${output_dir_random}/knowledge_base_${i}-${j}_random.txt"
+        clingo --outf=2 --quiet=1 "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c random=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
+    done
+done
 
 
