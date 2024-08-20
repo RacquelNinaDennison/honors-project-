128f62d2f6fd948dc2a9776f700a8241cd441066
RacquelNinaDennison <racquelnina@gmail.com>
2024-08-20 14:14:56 +0200

    Changed the specs

--- run-test.sh
+++ run-test.sh
@@ -11,7 +11,7 @@ mkdir -p "$output_dir_linear"
 mkdir -p "$output_dir_random"
 
 for i in $(seq 50 25 200); do
-    for j in $(seq 10 5 70); do
+    for j in $(seq 1 1 20); do
         output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear_tweety.txt"
         clingo --outf=2 --quiet=1 --configuration=tweety "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
         output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform_tweety.txt"
@@ -22,7 +22,7 @@ for i in $(seq 50 25 200); do
 done
 
 for i in $(seq 50 25 200); do
-    for j in $(seq 10 5 70); do
+     for j in $(seq 1 1 20); do
         output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear_jumpy.txt"
         clingo --outf=2 --quiet=1 --configuration=jumpy "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
         output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform_jumpy.txt"
@@ -34,7 +34,7 @@ done
 
 
 for i in $(seq 50 25 200); do
-    for j in $(seq 10 5 70); do
+     for j in $(seq 1 1 20); do
         output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear-heuristic-usc.txt"
         clingo --outf=2 --quiet=1 --opt-strategy=usc "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
         output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform-heuristic-usc.txt"
@@ -46,7 +46,7 @@ done
 
 
 for i in $(seq 50 25 200); do
-    for j in $(seq 10 5 70); do
+      for j in $(seq 1 1 20); do
         output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear-heuristic-bb.txt"
         clingo --outf=2 --quiet=1 --opt-strategy=bb "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
         output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform-heuristic-bb.txt"
@@ -59,7 +59,7 @@ done
 
 
 for i in $(seq 50 25 200); do
-    for j in $(seq 10 5 70); do
+     for j in $(seq 1 1 20); do
         output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear.txt"
         clingo --outf=2 --quiet=1 "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
         output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform.txt"
