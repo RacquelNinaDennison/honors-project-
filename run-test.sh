837968934c1b2dfc35309d534b7a763318468c07
Racquel Dennison <dnnrac003@nightmare.cs.uct.ac.za>
2024-08-21 16:53:17 +0000

    Added the experimental stuff

--- run-test.sh
+++ run-test.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
 
-output_dir_uniform="results_knowledge_base_uniform_experiment_fixed"
-output_dir_linear="results_knowledge_base_linear_experiment_fixed"
-output_dir_random="results_knowledge_base_random_experiment_fixed"
+output_dir_uniform="results_knowledge_base_uniform_experiment_fixed_jumpy"
+output_dir_linear="results_knowledge_base_linear_experiment_fixed_jumpy"
+output_dir_random="results_knowledge_base_random_experiment_fixed_jumpy"
 # Create output directory if it does not exist
 
 mkdir -p "$output_dir_uniform"
 mkdir -p "$output_dir_linear"
 mkdir -p "$output_dir_random"
 
-for i in $(seq 50 25 200); do
-    for j in $(seq 1 1 20); do
-        output_file="${output_dir_linear}/knowledge_base_${i}-${j}_linear_tweety.txt"
-        clingo --outf=2 --quiet=1 --configuration=tweety "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c linear=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
-        output_file="${output_dir_uniform}/knowledge_base_${i}-${j}_uniform_tweety.txt"
-        clingo --outf=2 --quiet=1 --configuration=tweety "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c uniform=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
-        output_file="${output_dir_random}/knowledge_base_${i}-${j}_random_tweety.txt"
-        clingo --outf=2 --quiet=1 --configuration=tweety "knowledge-base-instances.lp" -c amount_of_ranks=$j -c amount_of_statements=$i -c random=1 "knowledge_base_problem_class_2.lp" "functions.lp" > "${output_file}"
-    done
-done
 
 for i in $(seq 50 25 200); do
      for j in $(seq 1 1 20); do
