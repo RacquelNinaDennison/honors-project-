4e26aca13479d85a4983e3205bfdac4ce8fd2a4b
RacquelNinaDennison <racquelnina@gmail.com>
2024-09-09 20:04:57 +0200

    Added more testing

--- testing_rational_closure/run-rational-closure.py
+++ testing_rational_closure/run-rational-closure.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-import unittest
+import json
 
 class RunRationalClosureTest():
     def __init__(self) -> None:
@@ -18,12 +18,15 @@ class RunRationalClosureTest():
         result = subprocess.run(clingo_command, capture_output=True, text=True)
         with open(outputFile, "w") as file:
             file.write(result.stdout)
+    def read_query(self,queryFileName):
+        with open(queryFileName,"r+") as file:
+            jsonFile = json.load(file)
+            print(jsonFile["Call"][0]["Witnesses"][0]["Value"])
 
-
-def main(knowledge_base_rank, query, queryFileName, subdirectory):
+def main(knowledge_base_rank, query,queryfilename, subdirectory):
     if not os.path.exists(subdirectory):
         os.makedirs(subdirectory)
-    queryFilePath = os.path.join(subdirectory, queryFileName)
+    queryFilePath = os.path.join(subdirectory, queryfilename)
     with open(queryFilePath, "w+") as file:
         file.write(f"{query}.\n")
         for knowledge_statement in knowledge_base_rank:
@@ -32,4 +35,11 @@ def main(knowledge_base_rank, query, queryFileName, subdirectory):
     runRationalClosure = RunRationalClosureTest()
     outputFilePath = os.path.join(subdirectory, f"{query}.json")
     runRationalClosure.generate_rational_closure(queryFilePath, outputFilePath)
+    runRationalClosure.read_query(outputFilePath)
 
+def defining_test_data():
+    knowledge_base_rank = ["defeasible(a,b)", "defeasible(b,c)"]
+    query = "query(a,none)"
+    subdirectory = "rational_queries"
+    queryFileName = "query1.lp"
+    main(knowledge_base_rank, query,queryFileName, subdirectory)
